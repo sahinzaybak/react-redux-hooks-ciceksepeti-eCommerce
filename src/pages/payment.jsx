9e5e015a0b7b2f3e7702a2a8c655673f16a59848
Şahin Zaybak <sahinzaybak@teknasyon.com>
2021-04-27 15:32:53 +0300

    [fix] payment page basket lenght 0 added info box

--- src/pages/payment.jsx
+++ src/pages/payment.jsx
@@ -1,6 +1,8 @@
 import React, { PureComponent } from "react";
 import { connect } from "react-redux"; 
 import productListIco from '../assets/images/product-list-ico.svg'
+import {Link} from 'react-router-dom';
+import basketImg from "../assets/images/shopping-cart.svg";
 import '../assets/scss/basket.scss'
 import '../assets/scss/payment.scss'
 
@@ -21,6 +23,8 @@ class Payment extends PureComponent {
     return (
       <div className="basket mt-0 mt-md-5"> 
         <div className="custom-container">
+        {this.props.basketList.length > 0 ?
+          <>
           <div className="d-flex align-items-center">
             <img className="mr-2" src={productListIco} alt=""/>
             <h3>Ödeme Yap</h3>
@@ -33,6 +37,20 @@ class Payment extends PureComponent {
               <BasketSummary />
             </div>
           </div>
+          </>
+          :
+          <div className="d-flex justify-content-between align-items-center basket-no-product flex-column flex-md-row mt-4 mt-md-0">
+            <div className="d-flex mb-4 mb-md-0">
+              <img className="mr-4" src={basketImg} alt="" />
+                <h5>Sepetinizde ürün bulunmamaktadır.</h5>
+            </div>
+            <div>
+            <Link to="/urunler">
+            <button className="button start-shopping">Hemen Alışverişe Başla</button>
+            </Link>
+          </div>
+        </div> 
+        }
         </div>
       </div>
     );
