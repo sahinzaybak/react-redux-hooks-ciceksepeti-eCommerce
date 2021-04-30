29a32a8e3fb3adf921a0f83a1324df9af0ee5565
Şahin Zaybak <sahinzaybak@teknasyon.com>
2021-05-01 00:43:13 +0300

    [fix] changed footer text

--- src/components/footer/footer-description.jsx
+++ src/components/footer/footer-description.jsx
@@ -4,6 +4,9 @@ const FooterDescription = () => {
     return (
       <div className="footer-desc">
           <p className="footer-desc__text">Türkiyenin en büyük online çiçek sitesi ÇiçekBahçem ile sevdiklerinizi mutlu etmek çok kolay! Çiçek göndermenin ve “Mutlu etmenin adresi” ÇiçekBahçem ile sevdiklerinize özel günlerde online çiçek gönderebilirsiniz. Geniş çiçekçi ağı sayesinde çiçek siparişleriniz Türkiye’nin dört bir yanına hızlı ve sorunsuz şekilde gönderilir. Taze çiçeklerle hazırlanan mis kokulu şık çiçek aranjmanları arasından beğendiğiniz ürünü seçerek, hızlı bir şekilde online sipariş verebilirsiniz. Sevdiklerinizin doğum günü, yıldönümü gibi mutlu günlerinde onların sevincine ortak olmak için yapmanız gereken sadece birkaç tıkla sipariş vermek. Sevgililer Günü, Kadınlar Günü, Anneler Günü gibi özel günlerde de çiçek, hediye ve lezzetli bonnyFood ürünleriyle sevdiklerinizi mutlu edebilirsiniz. Çünkü mutlu etmenin adresi</p>
+          <p className="footer-desc__text mt-4"><strong>Bu bir test sitesidir! Hiç bir şekilde bir kuruma rakip olarak açılmış bir site olmamakla beraber, hiç bir şekilde ticari amacı yoktur.</strong>
+
+          </p>
       </div>
     )
   }
